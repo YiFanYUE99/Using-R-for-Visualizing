fcfb4939837ecaa5bb76a6fac8e9c46cc3a73cb3
YiFanYUE99 <yifanyue@outlook.com>
2023-04-18 17:10:42 +0800

    new_pic

---
+++ many_metabolites/boxplot.R
@@ -0,0 +1,41 @@
+library(ggplot2)
+library(ggpubr)
+getwd()
+setwd("D:/R_work/Using-R-for-Visualizing/met_pic_output")
+metabolites<-read.csv("D:/作业/pathway处理/All of the samples.csv",row.names=1,header=1, check.names = F)#check.names = F 防止数字开头的文件名乱码
+dim(metabolites)[2]
+
+metabolites<-t(metabolites)
+metabolites<-na.omit(metabolites)#na.omit只能删除有缺失值的整行
+metabolites<-as.data.frame(t(metabolites))
+metabolite<-metabolites[-1,]#新建只含keggid的表
+colnames(metabolite)<-metabolites[1,]#更换行名
+
+
+#将class由num转化为factor
+metabolite$Class<-factor(metabolite$Class,
+                          levels = c(0.00,10.00,1020.00,1.00,120.00,2.50,2.52,25.00),
+                          labels=c("0mM LG","10mM LG","10mM LG + 20uM A9","1mM LG","1mM LG + 20uM A9","2.5mM LG","2.5mM LG + 20uM A9","25mM LG"))
+
+for(i in 3:dim(metabolite)[2])
+{
+  title_name=colnames(metabolite[i])
+  png(filename=paste0(title_name,".png"),#好的批量命名方式paste0
+      width=1200,height=600,units="px",
+      pointsize = 12)#标签大小
+  plot<-ggplot(metabolite,aes(x=Class,y=metabolite[,i],fill=Class))+#以class为标准填充boxplot
+    geom_boxplot()+
+    theme(legend.title=element_blank(),
+          plot.title = element_text(hjust = 0.5,size = 20, face = "bold"),#标题居中，字体大小为20
+          axis.text=element_text(size=12,face = "bold"),#刻度
+          axis.title.x=element_text(size=14),axis.title.y=element_text(size=14))+#横纵坐标
+    labs(title=title_name,x='Class',y='Abundance')
+    print(plot)
+    dev.off()
+}
+
+
+
+
+
+  
--- pathview/pathview_test.R
+++ pathview/pathview_test.R
@@ -5,6 +5,8 @@ if (!require("BiocManager", quietly = TRUE))
 BiocManager::install("pathview")
 
 browseVignettes("pathview")
+help("pathview")
+
 
 #生成化合物数据
 sim.cpd.data<-read.csv("D:/作业/pathway处理/keggid.csv",row.names = 1, header = 1)
@@ -22,10 +24,15 @@ for (i in 1:39)
     
   }
 }
-cpd.data1<-sim.cpd.data[,1:2]
+cpd.data1<-sim.cpd.data[,1:2]#这部分是化合物keggid以及它在实验组的含量
 write.csv(cpd.data1, file = "D:/作业/pathway处理/ckeggid.csv")
 getwd()
 setwd("D:/R_work")
+
+
+
+#通过代谢物做代谢通路图，一次只能做一个通路上的
+#网页版网址https://pathview.uncc.edu/
 pv.out<-pathview(cpd.data = cpd.data1,#化合物数据 
                  pathway.id = "00250",
                  species = "hsa", 
